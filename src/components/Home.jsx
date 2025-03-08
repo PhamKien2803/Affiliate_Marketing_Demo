79a49306428e90919bd53c1fe9bd01e288186fb4
Kittiepie <thanhanle2711@gmail.com>
2025-03-08 18:14:24 +0700

    about page

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>Home Page</h2>
+      <p>This is just a placeholder </p>
+    </div>
+  );
+}
+
+export default Home;
